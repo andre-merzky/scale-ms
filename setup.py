70150f029f06281f9d8bb663fb8f3cf3fcefb1d2
M. Eric Irrgang <ericirrgang@gmail.com>
2020-02-19 19:07:37 +0300

    scale-ms-23: Incorporate LRO protocol.
    Move long-running operations semantics to conventional interface.
    To be compliant with Google API client tools, long-running operations
    should be serviced according to google/longrunning/operations.proto
    
    There are some additional dependencies that are needed to generate
    working code with a consistent API. It looks like we also need to
    import google/api/annotations.proto, google/api/client.proto, and
    google/rpc/status.proto, and provide licensing notices.
    
    Refs #23

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
 
+# TODO: Split server components to an "extras" optional subpackage.
 setup(
     name='scalems',
     version='0',
@@ -8,5 +9,8 @@ setup(
     license='',
     author='SCALE-MS team',
     author_email='',
-    description=''
+    description='',
+    requires=['googleapis-common-protos', # provides google.longrunning
+              'protobuf', # provides google.protobuf
+              ]
 )
